a7e67cc933e3beb40ae5d06b1992cc96dd7cb80c
Pavel <verevkinpavel1997@mail.ru>
2024-04-03 17:52:51 +0700

    Перевел комменты. Пишу логику теста1.

--- c/quadratic_equation.c
+++ c/quadratic_equation.c
@@ -1,8 +1,8 @@
 /*
- * / (КОРНЕВАЯ ДИРЕКТОРИЯ)
+ * c/ (DIRECTORY)
  *
  *
- * quadratic_equation.c (НАЗВАНИЕ ФАЙЛА)
+ * quadratic_equation.c (FILE NAME)
  *
  *		Module quadratic_equation for calc equations.
  *      By Verevkin Pavel Nikolaevich from University Science and Technology
@@ -11,7 +11,7 @@
  */
 
 #include <stdio.h>
-#include <math.h> // sqrt()
+#include <math.h>   // sqrt()
 #include <stdlib.h> // for malloc() and realloc(), atof()
 #include <string.h> // for strcmp(a,b)
 #include "quadratic_equation.h"
@@ -19,149 +19,201 @@
 int main(int argc, char *argv[])
 {
     int default_see = SEE_ROOTS;
-    //int command_line_use = PROGRAM_COMMAND_LINE_ARGS;
+    // int command_line_use = PROGRAM_COMMAND_LINE_ARGS;
     int see_path_line = SEE_PATH_FILE;
     int root_see = ROOT_SEE;
     int type_response = TYPE_RESPONSE;
 
-    // Для использования программы из командной строки
-    if(PROGRAM_COMMAND_LINE_ARGS == TRUEMY){
+    // To use the program from the command line
+    if (PROGRAM_COMMAND_LINE_ARGS == TRUEMY)
+    {
 
-        // Если мало аргументов (отсчет для argc с 1, для argv с 0)
-        if(argc <= 1) {fprintf(stderr,"Ошибка: неверное количество аргументов <=1\n"); return 1;}
-
-        // Справка по аргуметам и примеры
-        if(strcmp(argv[1], "-h") == TRUEMY)
+        // If there are few arguments (count for argc from 1, for argv from 0)
+        if (argc <= 1)
         {
-            puts("Это модуль quadratic_equation для вычисления ax^2+bx+c=0.");
-            puts("Вы вызвали справку help -h.");
+            fprintf(stderr, "Error: invalid number of arguments <=1\n");
+            return 1;
+        }
+
+        // Help for arguments and examples
+        if (strcmp(argv[1], "-h") == TRUEMY)
+        {
+            puts("This is the quadratic_equation module for calculating ax^2+bx+c=0.");
+            puts("You have called help -h.");
             puts("Example use: ./quadratic_equation 1 1 1");
-            puts("Аргументы:");
+            puts("Arguments:");
             puts("[1] = a [2] = b [3] = c");
-            puts("[4] = -y/-n -- показывать ли корни уравнения (по умолчанию - Да)");
+            puts("[4] = -y/-n -- whether to show the roots of the equation (by default, Yes)");
             puts("Example use: ./quadratic_equation 1 1 1 -y");
-            puts("[5] = -yp/-np -- Показывать ли полный путь к файлу (по умолчанию - Нет)");
+            puts("[5] = -yp/-np -- Whether to show the full path to the file (by default, No)");
             puts("Example use: ./quadratic_equation 1 1 1 -y -yp");
-            puts("[6] = -yr/-nr -- Показывать ли введенные a b c (по умолчанию - Нет)");
+            puts("[6] = -yr/-nr -- Whether to show the entered a b c (by default, No)");
             puts("Example use: ./quadratic_equation 1 1 1 -y -yp -yr");
-            puts("[7] = -ymas/-nmas -- Показывать тип ответа  ymas - [x1, x2] или [[c1r,c1i], [c2r,c2i]], nmas - в виде строки x1= x2= (по умолчанию -ymas)");
+            puts("[[7] = -mas/-mas -- Show the response type y max - [x1, x2] or [[c1,c1i], [c2,c2i]], nmas - as a string x1=.0 x2=.0 (default -ymaps)");
             puts("Example use: ./quadratic_equation 1 1 1 -y -yp -yr -ymas");
             return 2;
         }
 
-        // Проверка на допустимое количество аргументов
-        if(argc < 4 || argc > 8) { fprintf(stderr, "Неверное количество параметров : должно <4 or >8 : введено :%d\n", argc); return 3;}
-        
-        // Проверка на ввод и ошибка для аргумента [4]
-        if(argc == 5)
-        if(strcmp(argv[4], "-y") == TRUEMY) {;}
-        else if(strcmp(argv[4], "-n") == TRUEMY) {default_see = FALSEMY;}
-        else { fprintf(stderr, "Аргументом [4] может быть только -y/-n\n"); return 4;}
+        // Checking for the allowed number of arguments
+        if (argc < 4 || argc > 8)
+        {
+            fprintf(stderr, "Incorrect number of parameters : must be <4 or >8 : entered :%d\n", argc);
+            return 3;
+        }
 
-        // Проверка на ввод и ошибка для аргумента [5]       
-        if(argc == 6) 
-        if(strcmp(argv[5], "-yp") == TRUEMY) {
-            see_path_line = TRUEMY; 
-            puts(argv[0]);
+        // Checking for input and error for an argument [4]
+        if (argc == 5)
+            if (strcmp(argv[4], "-y") == TRUEMY)
+            {
+                ;
             }
-        else if(strcmp(argv[5], "-np") == TRUEMY) {see_path_line = FALSEMY;}
-        else { fprintf(stderr, "Аргументом [5] может быть только -yp/-np\n"); return 5;}       
-
-        // Проверка на ввод и ошибка для аргумента [6]
-        if(argc == 7)
-        if(strcmp(argv[6], "-yr") == TRUEMY) {
-            root_see = TRUEMY; 
-            puts(argv[1]); puts(argv[2]); puts(argv[3]);
+            else if (strcmp(argv[4], "-n") == TRUEMY)
+            {
+                default_see = FALSEMY;
             }
-        else if(strcmp(argv[6], "-nr") == TRUEMY) {root_see = FALSEMY;}
-        else { fprintf(stderr, "Аргументом [6] может быть только -yr/-nr\n"); return 6;}    
+            else
+            {
+                fprintf(stderr, "The argument [4] can only be -y/-n\n");
+                return 4;
+            }
 
-        // Проверка на ввод и ошибка для аргумента [7]
-        if(argc == 8)
-        if(strcmp(argv[7], "-ymas") == TRUEMY) {type_response = TRUEMY;}
-        else if(strcmp(argv[7], "-nmas") == TRUEMY) {type_response = FALSEMY;}
-        else { fprintf(stderr, "Аргументом [7] может быть только -ymas/-nmas\n"); return 7;}
+        // Checking for input and error for an argument [5]
+        if (argc == 6)
+            if (strcmp(argv[5], "-yp") == TRUEMY)
+            {
+                see_path_line = TRUEMY;
+                puts(argv[0]);
+            }
+            else if (strcmp(argv[5], "-np") == TRUEMY)
+            {
+                see_path_line = FALSEMY;
+            }
+            else
+            {
+                fprintf(stderr, "The argument [5] can only be -yp/-np\n");
+                return 5;
+            }
 
+        // Input check and error for the argument [6]
+        if (argc == 7)
+            if (strcmp(argv[6], "-yr") == TRUEMY)
+            {
+                root_see = TRUEMY;
+                puts(argv[1]);
+                puts(argv[2]);
+                puts(argv[3]);
+            }
+            else if (strcmp(argv[6], "-nr") == TRUEMY)
+            {
+                root_see = FALSEMY;
+            }
+            else
+            {
+                fprintf(stderr, "The argument [6] can only be -yr/-nr\n");
+                return 6;
+            }
+
+        // Input check and error for the argument [7]
+        if (argc == 8)
+            if (strcmp(argv[7], "-ymas") == TRUEMY)
+            {
+                type_response = TRUEMY;
+            }
+            else if (strcmp(argv[7], "-nmas") == TRUEMY)
+            {
+                type_response = FALSEMY;
+            }
+            else
+            {
+                fprintf(stderr, "The argument [7] can only be -y mas/-mas\n");
+                return 7;
+            }
     }
 
     // How use function
     int size = -1;
-    double* root = malloc(1 * sizeof(double));
-    if(root == NULL) 
+    double *root = malloc(1 * sizeof(double));
+    if (root == NULL)
     {
-        fprintf(stderr, "Ошибка: memory not allocated.\n");
+        fprintf(stderr, "Error: memory is not allocated.\n");
         exit(0);
     }
-    else 
+    else
     {
         size = solve_equation(atof(argv[1]), atof(argv[2]), atof(argv[3]), root);
 
         // Отображение
-        if(default_see == TRUEMY)
-        if(size == -1) fprintf(stderr, "Error: размер size = -1\n");
-        else if(size <= 2)
-        {   
-            if(type_response == FALSEMY)
+        if (default_see == TRUEMY)
+            if (size == -1)
+                fprintf(stderr, "Error: size = -1\n");
+            else if (size <= 2)
             {
-            printf("x1 = %f; ", root[0]);
-            printf("x2 = %f;\n", root[1]);
-            } else printf("%f %f\n", root[0], root[1]);
-        } else
-        {
-            if(type_response == FALSEMY)
+                if (type_response == FALSEMY)
+                {
+                    printf("x1 = %f; ", root[0]);
+                    printf("x2 = %f;\n", root[1]);
+                }
+                else
+                    printf("%f %f\n", root[0], root[1]);
+            }
+            else
             {
-            printf("c1 = %f + %f*i; ", root[0], root[1]);
-            printf("c2 = %f + %f*i\n", root[2], root[3]);
-            } else printf("%f %f %f %f\n", root[0], root[1], root[2], root[3]);
-        }
+                if (type_response == FALSEMY)
+                {
+                    printf("c1 = %f + %f*i; ", root[0], root[1]);
+                    printf("c2 = %f + %f*i\n", root[2], root[3]);
+                }
+                else
+                    printf("%f %f %f %f\n", root[0], root[1], root[2], root[3]);
+            }
     }
-    
+
     return 0;
 }
 
-//Function for calc ax^2+bx+c=0
-int solve_equation(double a, double b, double c, double* root)
+// Function for calc ax^2+bx+c=0
+int solve_equation(double a, double b, double c, double *root)
 {
-    struct data_eq data = {.isWhoI=-1, .type=-1};
+    struct data_eq data = {.isWhoI = -1, .type = -1};
 
-    if(a != 0) // Если уравнение квадратичное коэффициент 'a' должен быть отличен от нуля 
-    { 
+    if (a != 0) // If the equation is quadratic, the coefficient 'a' must be nonzero
+    {
         data.isWhoI = 1;
 
-        if(b != 0 && c != 0) // Если уравнение полное, то будет 3 случая
+        if (b != 0 && c != 0) // If the equation is complete, then there will be 3 cases
         {
-            data.discr = b*b - 4 * a * c;
+            data.discr = b * b - 4 * a * c;
 
-            if(data.discr > 0) // D>0 We have two real roots
+            if (data.discr > 0) // D>0 We have two real roots
             {
                 data.type = full_D_big_null;
                 data.n_root = two_root_real;
-                data.x1 = (-b + sqrt(data.discr)) / (2*a);
-                data.x2 = (-b - sqrt(data.discr)) / (2*a);
+                data.x1 = (-b + sqrt(data.discr)) / (2 * a);
+                data.x2 = (-b - sqrt(data.discr)) / (2 * a);
 
                 root = realloc(root, sizeof(double) * 2);
                 root[0] = data.x1;
                 root[1] = data.x2;
                 return data.n_root;
             }
-            else if(data.discr == 0) // D=0 We have one real root
+            else if (data.discr == 0) // D=0 We have one real root
             {
                 data.type = full_D_equal_null;
                 data.n_root = one_root_real;
-                data.x1 = -b / (2*a);
+                data.x1 = -b / (2 * a);
                 data.x2 = data.x1;
 
                 root = realloc(root, sizeof(double) * 1);
                 root[0] = data.x1;
                 return data.n_root;
             }
-            else if(data.discr < 0) // D<0 We have two complex root
+            else if (data.discr < 0) // D<0 We have two complex root
             {
                 data.type = full_D_less_null;
                 data.n_root = two_root_complex;
                 // real_part is the same at the two roots, image_part differs in sign
-                data.cmp1.real_part = -b / (2*a);
-                data.cmp1.image_part = sqrt(-data.discr) / (2*a);
+                data.cmp1.real_part = -b / (2 * a);
+                data.cmp1.image_part = sqrt(-data.discr) / (2 * a);
                 data.cmp2.real_part = data.cmp1.real_part;
                 data.cmp2.image_part = -1 * data.cmp1.image_part;
 
@@ -172,13 +224,13 @@ int solve_equation(double a, double b, double c, double* root)
                 root[3] = data.cmp2.image_part;
                 return data.n_root;
             }
-
-        } else // если уравнение не полное и один из коэффициентов b=0,c=0 равен нулю
+        }
+        else // if the equation is incomplete and one of the coefficients b=0,c=0 is zero
         {
-            if(b == 0 && c != 0)
+            if (b == 0 && c != 0)
             {
                 data.discr = -c / a;
-                if(data.discr > 0) // Будет два решения
+                if (data.discr > 0) // There will be two solutions
                 {
                     data.type = nofull_b_equal_null_D_big_null;
                     data.n_root = two_root_real;
@@ -206,7 +258,8 @@ int solve_equation(double a, double b, double c, double* root)
                     root[3] = data.cmp2.image_part;
                     return data.n_root;
                 }
-            } else if(c == 0 && b != 0)
+            }
+            else if (c == 0 && b != 0)
             {
                 data.type = nofull_c_equal_null;
                 data.n_root = two_root_real;
@@ -217,7 +270,8 @@ int solve_equation(double a, double b, double c, double* root)
                 root[0] = data.x1;
                 root[1] = data.x2;
                 return data.n_root;
-            } else // Если b=0 и c=0, то x12=0 
+            }
+            else // If b=0 and c=0, then x1=0 and x2=0
             {
                 data.type = nofull_c_and_b_equal_null;
                 data.n_root = one_root_real;
@@ -229,16 +283,11 @@ int solve_equation(double a, double b, double c, double* root)
                 return data.n_root;
             }
         }
-    } else 
-    { 
+    }
+    else
+    {
         data.isWhoI = 0;
-        fprintf(stderr, "Ошибка: это не квадратичное уравнение т.к. a=0\n");
+        fprintf(stderr, "Error: this is not a quadratic equation because a=0\n");
         exit(0);
     }
-
 }
-
-
-
-
-
--- c/quadratic_equation.h
+++ c/quadratic_equation.h
@@ -1,21 +1,28 @@
+/*
+ * c/ (DIRECTORY)
+ *
+ * quadratic_equation.h (FILE NAME)
+ * 
+*/ 
+
 #ifndef QUADRATIC_EQ_H
 #define QUADRATIC_EQ_H
 
 // defines
-#define TRUEMY 0 // определение что является правдой
-#define FALSEMY 1 // определение что является ложью
-#define SEE_ROOTS TRUEMY // Показывать чему равны корни
-#define PROGRAM_COMMAND_LINE_ARGS TRUEMY // Использовать из командной строки
-#define SEE_PATH_FILE TRUEMY // Показывать ли полный путь к файлу, который запускаем
-#define ROOT_SEE TRUEMY // Показывать введенные входные a b c
-#define TYPE_RESPONSE TRUEMY // Тип ответа  yes - [x1, x2] или [[c1r,c1i], [c2r,c2i]], no - в виде строки x1= x2=
+#define TRUEMY 0 // defining what is true
+#define FALSEMY 1 // defining what is a false
+#define SEE_ROOTS TRUEMY // Show what the roots are equal to
+#define PROGRAM_COMMAND_LINE_ARGS TRUEMY // Use from the command line
+#define SEE_PATH_FILE TRUEMY // Whether to show the full path to the file that we are launching
+#define ROOT_SEE TRUEMY // Show the entered input a b c
+#define TYPE_RESPONSE TRUEMY // The answer type is yes - [x1, x2] or [[c1,c1e], [c2,c2e]], no - as a string x1= x2=
 
 
 //Prototipe function for calc ax^2+bx+c=0
 int solve_equation(double a, double b, double c, double* root);
 
 // enums
-enum type_eq // тип уравнения
+enum type_eq // type of equation
 {
     full_D_big_null,
     full_D_equal_null,
@@ -27,7 +34,7 @@ enum type_eq // тип уравнения
     nofull_c_and_b_equal_null
 };
 
-enum num_root // количество корней
+enum num_root // number of roots
 {
     zero_root = 0,
     one_root_real = 1,
@@ -36,17 +43,17 @@ enum num_root // количество корней
 };
 
 // structs
-struct complex // Структура для представления комплексных чисел
+struct complex // A structure for representing complex numbers
 {
     double real_part;
     double image_part;
 };
 
-struct data_eq // все возможные данные уравнения
+struct data_eq // all possible equation data
 {
-    int isWhoI; // Если 1, то уравнение квадртаичное, если 0, то ошибка
-    int type; // тип уравнения type_eq
-    int n_root; // количество корней
+    int isWhoI; // If 1, then the equation is quadratic, if 0, then the error is
+    int type; // type of equation type_eq
+    int n_root; // number of roots
     double discr; // the number characterizing the equation
     double x1, x2; // real roots equation
     struct complex cmp1, cmp2; // complex roots equation
